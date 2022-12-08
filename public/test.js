d6976d9f790a727cefec5564f4526ca8ec74e04f
haoxiu wang <whxcctv@gmail.com>
2022-12-08 23:55:00 +0800

    update

--- public/test.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-const express = require('express');
-const mfunc = require('./mymodule');
-const app = express()
-const multipart = mfunc()
-app.use(multipart)
-http.createServer(app).listen(80)
