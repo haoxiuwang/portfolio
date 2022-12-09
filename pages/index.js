75caf5bfa712ca18d947d72b50acae828ba60b17
haoxiu wang <whxcctv@gmail.com>
2022-12-09 10:18:04 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -27,8 +27,9 @@ export default function Index({posts}) {
 
               return (
                 <div key={i} className="relative group card shadow-2xl col-span-2">
-                  <img src={image} alt="Tribute Page" className="object-cover h-full" />
+
                   <Link href={`/projects/${slug}`}>
+                  <img src={image} alt="Tribute Page" className="object-cover h-full" />
                     <div className="absolute bottom-0 left-0 right-0 lg:opacity-0 group-hover:opacity-100 bg-primary p-4 duration-300 text-primary-content">
                       <h2 className="font-bold lg:text-xl">{title}</h2>
 
