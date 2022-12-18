90bc6a6afc0b1c338079b9b2ad4988503827e229
haoxiu wang <whxcctv@gmail.com>
2022-12-18 19:52:35 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,18 @@ export default function Index() {
     <div  className="bg-sky-100">
       <div className="fixed">
         <F top="20px" left="20px" to={0}>
-        <div className="select-none flex place-content-center place-items-center space-x-5">
+        <div className="bg-rose-100 select-none flex place-content-start place-items-center space-x-5">
           {
             data.map((item,i)=>(
-              <div onClick={()=>setId(item.id)} className="flex-nowrap rounded-full p-2 flex place-content-evenly whitespace-nowrap space-x-2" key={i}>
-                  <img src={item.cover}/>
+              <div onClick={()=>setId(i)} className="flex-nowrap rounded-full p-2 flex flex-col place-content-start place-items-center whitespace-nowrap space-x-5" key={i}>
+                  <img width="150px" src={`/assets/images/${item.cover}`}/>
                   <span>{item.name}</span>
               </div>))
           }
         </div>
         </F>
-        <div className="mt-[100px] flex place-content-center">
-          <iframe className="w-full" id="iframe" frameBorder="no" border="0" marginWidth="0" marginHeight="0" width="330" height="4450" src={`https://music.163.com/outchain/player?type=4&id=${id}&auto=1&height=430&order=2`}></iframe>
+        <div className="mt-[300px] flex place-content-center">
+          <iframe className="w-full" id="iframe" frameBorder="no" border="0" marginWidth="0" marginHeight="0" width="330" height="4450" src={`https://music.163.com/outchain/player?type=4&id=${data[id].id}&auto=1&height=430&order=2`}></iframe>
         </div>
       </div>
     </div>
